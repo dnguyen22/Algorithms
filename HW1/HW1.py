0b3f97f8f595f1246d9e1e41d7c8dfdf596a94c7
dnguyen22 <don.nguyen22@gmail.com>
2017-08-20 17:57:54 -0500

    Add sort portion of function

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,28 +1,49 @@
 # HW1: Count Inversions
 
-def count_inversions(a_list, list_len):
-    if list_len == 0:
-        return 0
+
+def sort_and_count_inversions(a_list, list_len):
+    if list_len < 2:
+        return a_list, 0
     else:
         mid = len(a_list) // 2
         left_half = a_list[:mid]
         right_half = a_list[mid:]
 
-        left_count = count_inversions(left_half, len(left_half))
-        right_count = count_inversions(right_half, len(right_half))
-        split_count = count_split_inversions(a_list, mid)
+        left_tuple = sort_and_count_inversions(left_half, len(left_half))
+        right_tuple = sort_and_count_inversions(right_half, len(right_half))
+        split_tuple = merge_and_count_split_inversions(left_tuple[0], right_tuple[0])
 
-        return left_count + right_count + split_count
+        return split_tuple[0], left_tuple[1] + right_tuple[1] + split_tuple[1]
 
-def count_split_inversions(a_list, list_len):
-    return 0
 
-intArray = []
-with open('IntegerArray.txt') as f:
-    for line in f:
-        intArray.append(int(line))
+def merge_and_count_split_inversions(a_list, b_list):
+    sorted_list = []
+    count_inversions = 0
+    i = 0
+    j = 0
+
+    while i < len(a_list) and j < len(b_list):
+        if a_list[i] <= b_list[j]:
+            sorted_list.append(a_list[i])
+            i += 1
+        else:
+            sorted_list.append(b_list[j])
+            j += 1
+
+    while i < len(a_list):
+        sorted_list.append(a_list[i])
+        i += 1
+
+    while j < len(b_list):
+        sorted_list.append(b_list[j])
+        j += 1
+
+    return sorted_list, count_inversions
+
+intArray = [1, 3, 5, 2, 4, 6]
+# with open('IntegerArray.txt') as f:
+#     for line in f:
+#         intArray.append(int(line))
 
 print(intArray)
-print(len(intArray))
-print((len(intArray))//2)
-# print(count_inversions(intArray, len(intArray)))
+print(sort_and_count_inversions(intArray, len(intArray))[0])
