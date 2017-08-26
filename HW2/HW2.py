6c6299b52b29cd0b0b91adb0a24e8cec59ce319a
dnguyen22 <don.nguyen22@gmail.com>
2017-08-26 10:01:46 -0500

    First commit

---
+++ HW2/HW2.py
@@ -0,0 +1,11 @@
+# HW2: QuickSort
+
+intArray = []
+with open(''
+          'QuickSort.txt') as f:
+    for line in f:
+        intArray.append(int(line))
+
+# Print out array to make sure reading file worked
+print(intArray)
+print(len(intArray))
