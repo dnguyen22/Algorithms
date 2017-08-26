4223c5e00c14cb57c69b16ce46311f1f0e961636
dnguyen22 <don.nguyen22@gmail.com>
2017-08-26 18:10:58 -0500

    Add quick sort with comparisons count for pivot = median of first, last, middle elements

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -2,6 +2,12 @@
 
 
 def choose_pivot(a_list, start, end):
+    middle = ((end-1)-start)//2 + start
+    medians = [a_list[start], a_list[end-1], a_list[middle]]
+    return a_list.index(sorted(medians)[1])
+
+
+def set_final_element_pivot(a_list, start, end):
     return start
 
 
@@ -12,13 +18,15 @@ def set_final_element_pivot(a_list, start, end):
 def quick_sort(a_list, start, end):
     # Base case
     if start >= end:
-        # Return list and number of comparisons
+        # Return number of comparisons
         return 0
     else:
         # Set pivot as first element
-        #pivot = choose_pivot(a_list, start, end)
+        #pivot = set_first_element_pivot(a_list, start, end)
         # Set pivot as final element
-        pivot = set_final_element_pivot(a_list, start, end)
+        #pivot = set_final_element_pivot(a_list, start, end)
+        # Set pivot as median of medians
+        pivot = choose_pivot(a_list, start, end)
 
         # Partition list around pivot
         # Move pivot to start of list
@@ -29,7 +37,7 @@ def quick_sort(a_list, start, end):
             pivot = start
 
         # Index of list of the first value bigger than pivot out of values looked at in the for loop
-        pivot_boundary = start+1;
+        pivot_boundary = start + 1
 
         for index in range(start, end):
             if a_list[index] < a_list[pivot]:
@@ -55,6 +63,7 @@ with open(''
         intArray.append(int(line))
 
 # Print out array to make sure reading file worked
+# Not 150724; 159894
 print(intArray)
 print(len(intArray))
 print(quick_sort(intArray, 0, len(intArray)))
