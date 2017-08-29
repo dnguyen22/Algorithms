cd8689a86c101f9570abe6d3695fdf1a9e9375e4
dnguyen22 <don.nguyen22@gmail.com>
2017-08-28 22:26:38 -0500

    Refactor set to list

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -6,7 +6,6 @@ import random
 
 # Returns randomized calculation of min cut graph algorithm
 def calculate_min_cut(graph):
-    # removed_vertices = []
     # Iterate until only two vertices remain
     while len(graph) > 2:
         # Pop random vertex
@@ -14,26 +13,26 @@ def calculate_min_cut(graph):
         # Get edges of random vertex
         rand_edges = graph.pop(rand_vertex)
         # Randomly pick vertex to merge into from initial vertex edges
-        vertex_to_merge = random.choice(list(rand_edges))
+        vertex_to_merge = random.choice(rand_edges)
 
         # Merge random vertex into vertex_to_merge
         #
         # Remove rand_vertex from vertex_to_merge edges
-        graph[int(vertex_to_merge)].discard(str(rand_vertex))
+        graph[vertex_to_merge].remove(rand_vertex)
         # Remove vertex_to_merge from rand_vertex edges
-        rand_edges.discard(vertex_to_merge)
+        rand_edges.remove(vertex_to_merge)
 
         for edge in rand_edges:
             # Update edge of each vertex from rand_vertex to vertex_to_merge
-            graph[int(edge)].discard(str(rand_vertex))
-            graph[int(edge)].add(vertex_to_merge)
+            graph[edge].remove(rand_vertex)
+            graph[edge].append(vertex_to_merge)
             # Add rand_vertex edges to vertex_to_merge edges
-            graph[int(vertex_to_merge)].add(edge)
+            graph[vertex_to_merge].append(edge)
 
     return 0
 
 
-# Set up adjacency list as dictionary with keys as vertices, values as set of edges
+# Set up adjacency list as dictionary with keys as vertices, values as list of edges
 graph = {}
 with open('kargerMinCut.txt') as f:
     for line in f:
