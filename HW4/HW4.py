81460f5cf1202b80656ad3aecfb4c1f6240a9665
dnguyen22 <don.nguyen22@gmail.com>
2017-09-17 21:04:21 -0500

    Added signature for depth first search

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -13,10 +13,25 @@ def extract_data(file):
             graph.append(edge)
     return graph
 
+
+# Runs SCC algorithm on graph and returns sizes of SCCs
+def compute_scc_size(graph):
+    sizes = []
+    return sizes
+
+# Runs depth first search loop on entire graph (Kosaraju's Algorithm)
+def dfs_loop(graph):
+    return
+
+# Depth first search from starting node
+def dfs(graph, node):
+    return
+
 # Generate graph from edges text file
 graph = extract_data('SCC_test.txt')
 # Run SCC algorithm on graph, return sizes of SCC
 size_SCC = []
+size_SCC = compute_scc_size(graph)
 # Sort size_SCC from smallest to largest
 size_SCC.sort()
 # Print size of 5 largest SCCs
