3c5254d89242bdae27fe90c73dffbfc5d36946bb
dnguyen22 <don.nguyen22@gmail.com>
2017-10-04 16:58:26 -0500

    Set up heap and path length lists

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -1,31 +1,31 @@
 # HW5: Dijkstra's Algorithm
+import heapq
 
+# Max path length
+MAX_PATH_LENGTH = 1000000
 
 # Returns graph from text file of weighted edges. Graph is in the form of an adjacency list dictionary with vertices as
 #   keys and (end vertex, edge weight) tuples as values
 def extract_data(file):
-    graph = {}
+    adjacency_list = {}
     with open(file) as f:
         for line in f:
             split_line = line.split()
             # Set starting vertex as key with empty list as value
-            graph[int(split_line[0])] = []
+            adjacency_list[int(split_line[0])] = []
             # edges contains tuples of end vertices and edge weights
             edges = split_line[1:]
             for edge in edges:
                 split_edge = edge.split(',')
-                graph[int(split_line[0])].append((int(split_edge[0]), int(split_edge[1])))
+                adjacency_list[int(split_line[0])].append((int(split_edge[0]), int(split_edge[1])))
+    return adjacency_list
 
-    print(graph)
-            # Save edges as int tail and head
-            #tail = int(split_line[0])
-            #head = int(split_line[1])
+# Adjacency list graph
+graph = extract_data('dijkstraData.txt')
+# Heap data structure to hold edges
+heap = []
+# List to hold path lengths. Index i-1 holds path length from vertex 1 to vertex i
+path_length = [MAX_PATH_LENGTH] * len(graph)
 
-            # Add new node to graph; create new key if first time node appears
-            #if tail in graph:
-                #graph[tail].add(head)
-            #else:
-                #graph[tail] = set([head])
-    #return graph
+print(graph)
 
-extract_data('dijkstraData.txt')
