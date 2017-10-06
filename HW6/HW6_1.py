49be7e0389e31d53e3131b7c37b8d885cc4b215d
dnguyen22 <don.nguyen22@gmail.com>
2017-10-06 08:42:42 -0500

    Extract data

--- HW6/HW6_1.py
+++ HW6/HW6_1.py
@@ -1,17 +1,15 @@
 # HW6_1: 2-Sum Algorithm
 
-# Returns graph from text file of weighted edges. Graph is in the form of an adjacency list dictionary with vertices as
-#   keys and list of [edge weight, end vertex] as values
+
+# Returns set from text file of integer values. Since a set is implemented the same as dictionaries, we get O(1) inserts
+#   and O(1) lookups
 def extract_data(file):
-    adjacency_list = {}
+    input_integers = set()
     with open(file) as f:
         for line in f:
-            split_line = line.split()
-            # Set starting vertex as key with empty list as value
-            adjacency_list[int(split_line[0])] = []
-            # edges contains list of list of [end vertices and edge weights]
-            edges = split_line[1:]
-            for edge in edges:
-                split_edge = edge.split(',')
-                adjacency_list[int(split_line[0])].append([int(split_edge[1]), int(split_edge[0])])
-    return adjacency_list
+            input_integers.add(int(line))
+    return input_integers
+
+
+input_ints = extract_data('algo1-programming_prob-2sum.txt')
+print(input_ints)
