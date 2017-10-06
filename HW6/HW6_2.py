fcc558026058742f74b67b869a65212c312d44e6
dnguyen22 <don.nguyen22@gmail.com>
2017-10-06 15:24:43 -0500

    Add comments

--- HW6/HW6_2.py
+++ HW6/HW6_2.py
@@ -9,10 +9,13 @@ low_heap = []
 high_heap = []
 # Text file with integers
 file = 'Median.txt'
+# Index
+index = 0
 # Loop through file
 with open(file) as f:
     for line in f:
         next_value = int(line)
+        index = index + 1
 
         # Initial value when both heaps are empty
         if not low_heap and not high_heap:
@@ -22,7 +25,7 @@ with open(file) as f:
             if next_value > high_heap[0]:
                 heapq.heappush(high_heap, next_value)
             else:
-                heapq.heappush(low_heap, next_value)
+                heapq.heappush(low_heap, -next_value)
 
             # Balance heaps
             if len(high_heap) - len(low_heap) > 1:
@@ -30,6 +33,18 @@ with open(file) as f:
             elif len(low_heap) - len(high_heap) > 1:
                 heapq.heappush(high_heap, -(heapq.heappop(low_heap)))
 
-            # TODO: If index is odd, median is odd value from high heap or low heap.
-            #   If index is even, median is from low heap.
-            # TODO: Add median to medians list
+        # If index is odd, median is odd value from high heap or low heap.
+        #   If index is even, median is from low heap.
+        if index % 2 == 1:
+            if len(low_heap) > len(high_heap):
+                medians.append(-low_heap[0])
+            else:
+                medians.append(high_heap[0])
+        else:
+            medians.append(-low_heap[0])
+
+print("Medians: ")
+print(medians)
+print('Sum of Medians: ' + str(sum(medians)))
+print('Sum mod 10000: ' + str(sum(medians) % 10000))
+
