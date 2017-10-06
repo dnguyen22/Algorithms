97ce1b0726742ec4eb021b8ea6f12fa384114c20
dnguyen22 <don.nguyen22@gmail.com>
2017-10-06 11:48:14 -0500

    Add comments for next steps

--- HW6/HW6_2.py
+++ HW6/HW6_2.py
@@ -1,2 +1,24 @@
 # HW6_2: Median Maintenance Algorithm
+import heapq
 
+# Store medians of numbers up to index i
+medians = []
+# Heap to store all low values. Supports extract max
+low_heap = []
+# Heap to store all high values. Supports extract min
+high_heap = []
+# Text file with integers
+file = 'Median.txt'
+# Loop through file
+with open(file) as f:
+    for line in f:
+        next_value = int(line)
+
+        # Initial value when both heaps are empty
+        if not low_heap and not high_heap:
+            heapq.heappush(high_heap, next_value)
+        else:
+            # TODO: Add new value to high heap if greater than smallest value. Add to low heap if less than highest value
+            # TODO: Balance heaps
+            # TODO: If index is odd, median is odd value from high heap or low heap. If index is even, median is from low heap
+            # TODO: Add median to medians list
