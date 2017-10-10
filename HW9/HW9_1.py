7103cd943386c9cd6a7fe7a6b22d2b752babdfbd
dnguyen22 <don.nguyen22@gmail.com>
2017-10-10 11:06:07 -0500

    BFS to find min depth of tree

--- HW9/HW9_1.py
+++ HW9/HW9_1.py
@@ -1,6 +1,15 @@
 # HW9: Huffman Encoding - Greedy Algorithms
 import heapq
 
+
+# Node for Huffman Encoding tree
+class HuffmanNode:
+    def __init__(self, left = None, right = None, root = None):
+        self.l = left
+        self.r = right
+        self.root = root
+
+
 # Loops through file and adds vertices to UnionFind
 def extract_data(file):
     alphabet = []
@@ -23,3 +32,31 @@ for letter in letters:
     heapq.heappush(heap, letter)
 
 print(heap)
+while len(heap) > 1:
+    left = heapq.heappop(heap)
+    right = heapq.heappop(heap)
+    node = HuffmanNode(left[1], right[1])
+    heapq.heappush(heap,(left[0] + right[0], node))
+
+tree = heapq.heappop(heap)
+print(tree)
+
+
+# Breath first search to get min depth of tree
+def get_min_depth(root):
+    if isinstance(root.l, int) or isinstance(root.r, int):
+        return 1
+    left_child = get_min_depth(root.l) + 1
+    right_child = get_min_depth(root.r) + 1
+
+    if right_child > left_child:
+        return right_child
+    else:
+        return left_child
+
+
+print(get_min_depth(tree[1]))
+testnode = HuffmanNode(5, 9) # Depth of 1
+testnode2 = HuffmanNode(2, testnode) # Depth of 2
+testnode3 = HuffmanNode(testnode2, testnode) # Depth of 3
+print(get_min_depth(testnode3))
