2f0b4b7f9e46e2ded58e3d74f1079bc8a15ef118
dnguyen22 <don.nguyen22@gmail.com>
2017-10-10 12:51:13 -0500

    Dynamic programming to find weight set

--- HW9/HW9_2.py
+++ HW9/HW9_2.py
@@ -8,9 +8,16 @@ def extract_data(file):
         # Skip first line (header)
         j = f.readlines()[1:]
         for line in j:
-            weights.append((int(line), index))
-            index = index + 1
+            weights.append(int(line))
     return weights
 
 
 vertices = extract_data('mwis.txt')
+# Holds total weight of set, including set of 0 vertices
+set_weight = [0, vertices[0]]
+# Dynamic programming approach to find solution in O(n) time
+for i in range(2, len(vertices)+1):
+    set_weight.append(max(set_weight[i-1], set_weight[i-2] + vertices[i-1]))
+
+print(vertices)
+print(set_weight)
