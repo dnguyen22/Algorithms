7d9c65ec4ab49a383a498a87e5688b4b59a42471
dnguyen22 <don.nguyen22@gmail.com>
2017-10-10 14:22:52 -0500

    Extract data into dictionary

--- HW10/HW10.py
+++ HW10/HW10.py
@@ -1,15 +1,22 @@
 # HW10: Knapsack Problem - Dynamic Programming
 
-# Loops through file and adds items
+
+# Loops through file and adds items to dictionary for fast lookups
 def extract_data(file):
-    i = []
+    dictionary = dict()
+    index = 0
     with open(file) as f:
         # Skip first line (header)
         j = f.readlines()[1:]
         for line in j:
             split = line.split()
-            i.append((int(split[0]), int[split[1]]))
-    return i
+            # dictionary values are tuples of item (value, weight)
+            dictionary[index] = (int(split[0]), int(split[1]))
+            # Increment item index
+            index = index + 1
+    return dictionary
+
 
 # Items
 items = extract_data('knapsack1.txt')
+print(items)
