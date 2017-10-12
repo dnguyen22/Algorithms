a20092e6a651b35112602837bd01dc3fefa79570
dnguyen22 <don.nguyen22@gmail.com>
2017-10-12 08:41:10 -0500

    Extract data into dictionary

--- HW12/HW12.py
+++ HW12/HW12.py
@@ -1,4 +1,21 @@
 # HW12: Traveling Salesman Problem - Nearest Neighbor Heuristic
 
 
+# Loops through file and adds locations to dictionary
+def extract_data(file):
+    dictionary = dict()
+    with open(file) as f:
+        text = f.readlines()
+        # Extract data from header
+        num_cities = int(text[0])
+        # Skip first line (header)
+        for line in text[1:]:
+            split = line.split()
+            # dictionary values are tuples of item (x-coordinate, y-coordinate)
+            dictionary[int(split[0])] = (float(split[1]), float(split[2]))
+    return dictionary, num_cities
 
+
+# Cities
+cities, NUM_CITIES = extract_data('nn.txt')
+print(cities)
